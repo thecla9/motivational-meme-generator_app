67494c5215873106203fcd87a321b132e3eca2e7
THECLA <preciouschy4christ@yahoo.com>
2022-09-11 03:29:02 +0100

    Update meme.py

--- src/meme.py
+++ src/meme.py
@@ -2,22 +2,19 @@ import argparse
 import os
 import random
 
-
-
-# @TODO Import your Ingestor and MemeEngine classes
+# Import Ingestor and MemeEngine classes
 from memegenerator.memeengine import MemeEngine
 from quoteengine.ingestor import Ingestor
 from quoteengine.quotes import QuoteModel
 
 
-
 def generate_meme(path=None, body=None, author=None):
-    """ Generate a meme given an path and a quote """
+    """Generate a meme given an path and a quote."""
     img = None
     quote = None
 
     if path is None:
-        images = os.getcwd() +"./src/_data/photos/dog/"
+        images = os.getcwd() + "./src/_data/photos/dog/"
         imgs = []
         for root, dirs, files in os.walk(images):
             imgs = [os.path.join(root, name) for name in files]
@@ -35,7 +32,7 @@ def generate_meme(path=None, body=None, author=None):
         for f in [Ingestor.parse(file) for file in quote_files]:
             for a in f or []:
                 quotes.append(a)
-        
+
         quote = random.choice(quotes)
     else:
         if author is None:
@@ -48,10 +45,7 @@ def generate_meme(path=None, body=None, author=None):
 
 
 if __name__ == "__main__":
-    # @TODO Use ArgumentParser to parse the following CLI arguments
-    # path - path to an image file
-    # body - quote body to add to the image
-    # author - quote author to add to the image
+
     parser = argparse.ArgumentParser(description="Meme generator")
     parser.add_argument('-p', '--path')
     parser.add_argument('-b', '--body')
