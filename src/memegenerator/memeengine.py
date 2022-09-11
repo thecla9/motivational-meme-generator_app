0a4c9562d187261acb93cf4cddeddcd725146ba7
THECLA <preciouschy4christ@yahoo.com>
2022-09-11 03:30:25 +0100

    Update memeengine.py

--- src/memegenerator/memeengine.py
+++ src/memegenerator/memeengine.py
@@ -8,17 +8,17 @@ from random import randint
 
 
 class MemeEngine:
-    """Class to create memes from images and quotes"""
+    """Class to create memes from images and quotes."""
 
     def __init__(self, output_dir: str) -> None:
         """Initialize a meme generator object."""
+
         self.output_dir = output_dir
 
     @classmethod
     def load_image(cls, path: str, size: int) -> Image:
-        '''
-        Load and return croped image if bigger then 500 px
-        '''
+        """Load and return croped image if bigger then 500 px."""
+
         im = Image.open(path)
         width, height = im.size
         if width > size or height > size:
@@ -44,9 +44,8 @@ class MemeEngine:
 
     @classmethod
     def place_text(cls, im: Image, quote, author):
-        '''
-        Place text on a random position on the image
-        '''
+        """Place text on a random position on the image."""
+
         draw = ImageDraw.Draw(im)
         font = ImageFont.truetype(
             "arial.ttf", 15)
@@ -68,14 +67,16 @@ class MemeEngine:
         return im
 
     def make_meme(self, path, quote, author) -> str:
-        '''
+        """
         Place text of choice on a picture of choice
-        OR Place some random text on a random pic
-        from the database
-        @param path: path to the picture
-        @param quote: str
-        @param author: str
-        '''
+         OR Place some random text on a random pic
+         from the database
+         @param path: path to the picture
+         @param quote: str
+         @param author: str
+
+         """
+
         im = self.load_image(path, 500)
         extension = path.split('/')[-1].split('.')[1]
         save_path = self.output_dir + extension
